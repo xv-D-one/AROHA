a835f8a93ec649af41f108391ee0e4f6021c1b2e
Dhanvin Ranjith <dhanvinranjith@gmail.com>
2026-03-08 15:13:40 +0530

    Add files via upload

---
+++ hospital_analytics_questions.sql
@@ -0,0 +1,29 @@
+-- Connect to database (MySQL only)
+USE hospital_db;
+
+-- OBJECTIVE 1: ENCOUNTERS OVERVIEW
+
+-- a. How many total encounters occurred each year?
+
+-- b. For each year, what percentage of all encounters belonged to each encounter class
+-- (ambulatory, outpatient, wellness, urgent care, emergency, and inpatient)?
+
+-- c. What percentage of encounters were over 24 hours versus under 24 hours?
+
+-- OBJECTIVE 2: COST & COVERAGE INSIGHTS
+
+-- a. How many encounters had zero payer coverage, and what percentage of total encounters does this represent?
+
+-- b. What are the top 10 most frequent procedures performed and the average base cost for each?
+
+-- c. What are the top 10 procedures with the highest average base cost and the number of times they were performed?
+
+-- d. What is the average total claim cost for encounters, broken down by payer?
+
+-- OBJECTIVE 3: PATIENT BEHAVIOR ANALYSIS
+
+-- a. How many unique patients were admitted each quarter over time?
+
+-- b. How many patients were readmitted within 30 days of a previous encounter?
+
+-- c. Which patients had the most readmissions?
